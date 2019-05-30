374307598d743668ec1097c6f26e397f5d772c51
Jian-Hua, He <game09116@gmail.com>
2019-05-30 10:52:52 +0800

    Pass the test case

--- interview/medianunsort/main.go
+++ interview/medianunsort/main.go
@@ -2,21 +2,14 @@ package medianunsort
 
 func FindMedian(s []float64) float64 {
 	sum := sum(s)
-	avg := sum / float64(len(s))
-	diff := absVal(avg, s[0])
+	length := len(s)
+	avg := sum / float64(length)
 
-	result := 0.0
-	for _, v := range s {
-		d := absVal(avg, v)
-		if d >= diff {
-			continue
-		}
-
-		result = v
-		diff = d
+	if length%2 == 1 {
+		return findInOdd(s, avg)
 	}
 
-	return result
+	return findInEven(s, avg)
 }
 
 func sum(s []float64) float64 {
@@ -35,4 +28,54 @@ func absVal(n1 float64, n2 float64) float64 {
 	}
 
 	return n2 - n1
+}
+
+func findInOdd(s []float64, avg float64) float64 {
+	result := 0.0
+	diff := absVal(avg, s[0])
+
+	for _, v := range s {
+		d := absVal(avg, v)
+		if d >= diff {
+			continue
+		}
+
+		result = v
+		diff = d
+	}
+
+	return result
+}
+
+func findInEven(s []float64, avg float64) float64 {
+	arr := s
+
+	index := 0
+	diff1 := absVal(avg, arr[0])
+	n1 := arr[0]
+	for i, v := range arr {
+		d := absVal(avg, v)
+		if d >= diff1 {
+			continue
+		}
+
+		n1 = v
+		index = i
+		diff1 = d
+	}
+
+	arr = append(arr[0:index], arr[index+1:]...)
+	diff2 := absVal(avg, arr[0])
+	n2 := arr[0]
+	for _, v := range arr {
+		d := absVal(avg, v)
+		if d >= diff2 {
+			continue
+		}
+
+		n2 = v
+		diff2 = d
+	}
+
+	return (n1 + n2) / 2
 }
