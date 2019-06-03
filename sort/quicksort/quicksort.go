815d6fe80b35df3122d52a7fd59f78ac4c2fcc62
Jian-Hua, He <game09116@gmail.com>
2019-06-03 16:04:56 +0800

    Implement quick sort

--- sort/quicksort/quicksort.go
+++ sort/quicksort/quicksort.go
@@ -1,21 +1,25 @@
 package quicksort
 
-import "fmt"
-
 func Sort(s []interface{}, f func(interface{}, interface{}) bool) []interface{} {
-	pivot := len(s) - 1
+	partition(s, 0, len(s)-1, f)
+	return s
+}
 
-	j := 0
-	for i := 0; i < len(s)-1; i += 1 {
-		if f(s[pivot], s[i]) {
+func partition(s []interface{}, start int, end int, f func(interface{}, interface{}) bool) {
+	if end-start <= 0 {
+		return
+	}
+
+	j := start
+	for i := start; i < end; i += 1 {
+		if f(s[end], s[i]) {
 			s[i], s[j] = s[j], s[i]
 			j += 1
 		}
 	}
 
-	s[pivot], s[j] = s[j], s[pivot]
+	s[end], s[j] = s[j], s[end]
 
-	fmt.Println(s)
-
-	return s
+	partition(s, j+1, end, f)
+	partition(s, start, j-1, f)
 }
