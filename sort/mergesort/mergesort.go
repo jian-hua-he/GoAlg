0bf4198925904391a5168f99169c843f798cd2c4
Jian-Hua, He <game09116@gmail.com>
2019-06-04 14:21:00 +0800

    Add new test for merge function

--- sort/mergesort/mergesort.go
+++ sort/mergesort/mergesort.go
@@ -1,5 +1,49 @@
 package mergesort
 
+import (
+	"fmt"
+	"math"
+)
+
 func Sort(s []interface{}, f func(interface{}, interface{}) bool) []interface{} {
-	return s
+	arr := append([]interface{}{}, s...)
+	return mergeSort(arr, f)
+}
+
+func mergeSort(s []interface{}, f func(interface{}, interface{}) bool) []interface{} {
+	if len(s) <= 1 {
+		return s
+	}
+
+	middle := int(math.Floor(float64(len(s) / 2)))
+	left := mergeSort(s[:middle], f)
+	right := mergeSort(s[middle:], f)
+	result := merge(left, right, f)
+
+	return result
+}
+
+func merge(left []interface{}, right []interface{}, f func(interface{}, interface{}) bool) []interface{} {
+	result := make([]interface{}, 0)
+
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if f(left[i], right[j]) {
+			result = append(result, left[i])
+			i += 1
+		} else {
+			result = append(result, right[j])
+			j += 1
+		}
+		fmt.Println("=====")
+		fmt.Printf("Result: %+v\n", result)
+		fmt.Printf("Left: %+v\n", i)
+		fmt.Printf("Right: %+v\n", j)
+		fmt.Println("=====")
+	}
+
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
 }
