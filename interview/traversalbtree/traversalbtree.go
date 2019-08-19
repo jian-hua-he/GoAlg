44207e830056d858616f01bb4c59f1e411bcd507
Jian-Hua, He <game09116@gmail.com>
2019-08-19 11:03:28 +0800

    Add recursion solution

--- interview/traversalbtree/traversalbtree.go
+++ interview/traversalbtree/traversalbtree.go
@@ -7,5 +7,19 @@ type BTree struct {
 }
 
 func solution(t *BTree) int {
-	return 0
+	result := recursion(t, t.Value, 0)
+	return result
+}
+
+func recursion(t *BTree, root int, count int) int {
+	result := count
+	if t == nil {
+		return result
+	}
+
+	if t.Value >= root {
+		result += 1
+	}
+
+	return result + recursion(t.Left, root, 0) + recursion(t.Right, root, 0)
 }
