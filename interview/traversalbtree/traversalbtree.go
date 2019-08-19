8d3fdb5d3b5714b1094b5468635fac34d9e36496
Jian-Hua, He <game09116@gmail.com>
2019-08-19 09:37:05 +0800

    Add test case for traversal btree

---
+++ interview/traversalbtree/traversalbtree.go
@@ -0,0 +1,11 @@
+package traversalbtree
+
+type BTree struct {
+	Value int
+	Left  *BTree
+	Right *BTree
+}
+
+func solution(t *BTree) int {
+	return 0
+}
---
+++ interview/traversalbtree/traversalbtree_test.go
@@ -0,0 +1,34 @@
+package traversalbtree
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCase1(t *testing.T) {
+	bt := &BTree{Value: 5}
+	bt.Left = &BTree{Value: 2}
+	bt.Right = &BTree{Value: 3}
+	bt.Left.Left = &BTree{Value: 20}
+	bt.Left.Right = &BTree{Value: 21}
+	bt.Right.Left = &BTree{Value: 10}
+
+	got := solution(bt)
+	want := 4
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Data was incorrect, got %#v, want %#v", got, want)
+	}
+}
+
+func TestCase2(t *testing.T) {
+	bt := &BTree{Value: 8}
+	bt.Left = &BTree{Value: 3}
+	bt.Right = &BTree{Value: 2}
+	bt.Left.Left = &BTree{Value: 8}
+
+	got := solution(bt)
+	want := 2
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Data was incorrect, got %#v, want %#v", got, want)
+	}
+}
