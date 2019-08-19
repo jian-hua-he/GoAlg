16cfc0fd6462ba5c4d3d14cfde024f0f53f76745
Jian-Hua, He <game09116@gmail.com>
2019-08-19 12:11:57 +0800

    Add iterate method

--- interview/traversalbtree/traversalbtree.go
+++ interview/traversalbtree/traversalbtree.go
@@ -1,5 +1,9 @@
 package traversalbtree
 
+import (
+	"fmt"
+)
+
 type BTree struct {
 	Value int
 	Left  *BTree
@@ -22,4 +26,40 @@ func recursion(t *BTree, root int, count int) int {
 	}
 
 	return result + recursion(t.Left, root, 0) + recursion(t.Right, root, 0)
+}
+
+// Not finish
+func iterate(t *BTree) {
+	if t == nil {
+		return
+	}
+
+	parents := []*BTree{}
+	nums := []int{}
+
+	curr := t
+	hasLeft := true
+	for len(parents) > 0 || hasLeft {
+		if hasLeft {
+			nums = append(nums, curr.Value)
+			parents = append(parents, curr)
+		}
+
+		if curr.Left != nil && hasLeft {
+			curr = curr.Left
+			continue
+		}
+
+		if curr.Right != nil {
+			curr = curr.Right
+			hasLeft = true
+			continue
+		}
+
+		hasLeft = false
+		curr = parents[len(parents)-1]
+		parents = parents[:len(parents)-1]
+	}
+
+	fmt.Println(nums)
 }
