4ffd9ceec6eca3e171700f9fbe3eb8664f696e7f
Jian-Hua, He <game09116@gmail.com>
2019-08-20 10:50:55 +0800

    Use iterate loop to handle btree

--- interview/traversalbtree/traversalbtree.go
+++ interview/traversalbtree/traversalbtree.go
@@ -1,9 +1,5 @@
 package traversalbtree
 
-import (
-	"fmt"
-)
-
 type BTree struct {
 	Value int
 	Left  *BTree
@@ -11,7 +7,8 @@ type BTree struct {
 }
 
 func solution(t *BTree) int {
-	result := recursion(t, t.Value, 0)
+	// result := recursion(t, t.Value, 0)
+	result := iterate(t, t.Value)
 	return result
 }
 
@@ -28,38 +25,49 @@ func recursion(t *BTree, root int, count int) int {
 	return result + recursion(t.Left, root, 0) + recursion(t.Right, root, 0)
 }
 
-// Not finish
-func iterate(t *BTree) {
+func iterate(t *BTree, root int) int {
+	count := 0
 	if t == nil {
-		return
+		return count
 	}
 
 	parents := []*BTree{}
-	nums := []int{}
-
+	direction := []bool{} // Right: true, Left: false
 	curr := t
-	hasLeft := true
-	for len(parents) > 0 || hasLeft {
-		if hasLeft {
-			nums = append(nums, curr.Value)
+	for curr != nil {
+		if curr.Left != nil {
 			parents = append(parents, curr)
-		}
-
-		if curr.Left != nil && hasLeft {
+			direction = append(direction, false)
 			curr = curr.Left
 			continue
 		}
 
 		if curr.Right != nil {
+			parents = append(parents, curr)
+			direction = append(direction, true)
 			curr = curr.Right
-			hasLeft = true
 			continue
 		}
 
-		hasLeft = false
+		if curr.Value >= root {
+			count += 1
+		}
+
+		if len(parents) == 0 {
+			curr = nil
+			continue
+		}
+
 		curr = parents[len(parents)-1]
 		parents = parents[:len(parents)-1]
+		goRight := direction[len(direction)-1]
+		direction = direction[:len(direction)-1]
+		if goRight {
+			curr.Right = nil
+		} else {
+			curr.Left = nil
+		}
 	}
 
-	fmt.Println(nums)
+	return count
 }
