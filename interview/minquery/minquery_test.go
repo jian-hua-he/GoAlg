1a0c7dfa85effc988158847aa00ecc20d0e91911
Jian-Hua, He <game09116@gmail.com>
2019-09-20 10:54:10 +0800

    Add first case

--- interview/minquery/minquery_test.go
+++ interview/minquery/minquery_test.go
@@ -1,9 +1,17 @@
 package minquery
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestCase1(t *testing.T) {
-	t.Errorf("Not implement")
+	keywords := []string{"hello", "happy", "great"}
+	maxLen := 50
+	want := []string{"hello OR happy OR great"}
+	got := generateQueries(keywords, maxLen)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Data was incorrect, keywords %#v, max len %#v, got %#v, want %#v", keywords, maxLen, got, want)
+	}
 }
