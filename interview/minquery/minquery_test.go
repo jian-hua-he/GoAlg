80ddcf444a59d4737f4f4a4fcd203d3dbb7b12b6
Jian-Hua, He <game09116@gmail.com>
2019-09-20 12:01:12 +0800

    Add more test case

--- interview/minquery/minquery_test.go
+++ interview/minquery/minquery_test.go
@@ -8,8 +8,8 @@ import (
 func TestCase1(t *testing.T) {
 	keywords := []string{"hello", "happy", "great"}
 	maxLen := 50
-	want := []string{"hello OR happy OR great"}
-	got := generateQueries(keywords, maxLen)
+	want := 1
+	got := len(generateQueries(keywords, maxLen))
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Data was incorrect, keywords %#v, max len %#v, got %#v, want %#v", keywords, maxLen, got, want)
@@ -19,8 +19,19 @@ func TestCase1(t *testing.T) {
 func TestCase2(t *testing.T) {
 	keywords := []string{"foo", "fuzz", "bar", "buzz"}
 	maxLen := 10
-	want := []string{"foo OR bar", "fuzz", "buzz"}
-	got := generateQueries(keywords, maxLen)
+	want := 3
+	got := len(generateQueries(keywords, maxLen))
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Data was incorrect, keywords %#v, max len %#v, got %#v, want %#v", keywords, maxLen, got, want)
+	}
+}
+
+func TestCase3(t *testing.T) {
+	keywords := []string{"a", "bb", "ccc", "dddd", "eeeee", "ffffff", "ggggggg", "hhhhhhhh", "iiiiiii", "jjjjjj", "kkkkk", "llll", "mmm", "nn", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	maxLen := 13
+	want := 11
+	got := len(generateQueries(keywords, maxLen))
 
 	if !reflect.DeepEqual(got, want) {
 		t.Fatalf("Data was incorrect, keywords %#v, max len %#v, got %#v, want %#v", keywords, maxLen, got, want)
