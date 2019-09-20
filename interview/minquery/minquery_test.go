13f58cb55bd2ebf12a2f642bc146d2be7bdfd9bf
Jian-Hua, He <game09116@gmail.com>
2019-09-20 11:02:39 +0800

    Add test case 2

--- interview/minquery/minquery_test.go
+++ interview/minquery/minquery_test.go
@@ -15,3 +15,14 @@ func TestCase1(t *testing.T) {
 		t.Fatalf("Data was incorrect, keywords %#v, max len %#v, got %#v, want %#v", keywords, maxLen, got, want)
 	}
 }
+
+func TestCase2(t *testing.T) {
+	keywords := []string{"foo", "fuzz", "bar", "buzz"}
+	maxLen := 6
+	want := []string{"foo OR bar", "fuzz", "buzz"}
+	got := generateQueries(keywords, maxLen)
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("Data was incorrect, keywords %#v, max len %#v, got %#v, want %#v", keywords, maxLen, got, want)
+	}
+}
