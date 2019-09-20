0bf85967019b21423da25e72ac9bff897a34596c
Jian-Hua, He <game09116@gmail.com>
2019-09-20 11:52:39 +0800

    Add implementation

--- interview/minquery/minquery.go
+++ interview/minquery/minquery.go
@@ -1,7 +1,12 @@
 package minquery
 
 import (
+	"fmt"
 	"sort"
+)
+
+const (
+	JOIN_STRING = " OR "
 )
 
 type ByLen []string
@@ -12,7 +17,70 @@ func (s ByLen) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
 
 func generateQueries(keywords []string, maxCharLen int) []string {
 	result := []string{}
+
+	// Sort it by string length
 	sort.Sort(ByLen(keywords))
+
+	for len(keywords) != 0 {
+		// Find the greatest one and remove it from array
+		greatest := keywords[len(keywords)-1]
+		keywords = keywords[:len(keywords)-1]
+
+		// Compare to maxCharLen
+		// If it is equal to maxCharLen then add it to result
+		if len(greatest) == maxCharLen {
+			result = append(result, greatest)
+			continue
+		}
+
+		// If the length of greatest string add length of join string
+		// and it equal to maxCharLen then add it to result
+		if (len(greatest) + len(JOIN_STRING)) == maxCharLen {
+			result = append(result, greatest)
+			continue
+		}
+
+		// If it is too big then do nothing
+		if (len(greatest) + len(JOIN_STRING)) > maxCharLen {
+			continue
+		}
+
+		// If it still has space to join
+		// Find the string as possible as we can
+		joint := ""
+		for i := len(keywords) - 1; i >= 0; i -= 1 {
+			// Calculate the max every time
+			max := maxCharLen - len(greatest) - len(joint)
+
+			// If it equal to max then remove it from array
+			// and add it to joint. break the loop
+			if len(keywords[i])+len(JOIN_STRING) == max {
+				joint = fmt.Sprintf("%s%s", JOIN_STRING, keywords[i])
+				keywords = append(keywords[:i], keywords[i+1:]...)
+				break
+			}
+
+			// If it bigger than max. Just continue
+			if len(keywords[i])+len(JOIN_STRING) > max {
+				continue
+			}
+
+			// If it less than max then remove it from array
+			// and add it to joint. Continue to find another query
+			joint = fmt.Sprintf("%s%s%s", joint, JOIN_STRING, keywords[i])
+			keywords = append(keywords[:i], keywords[i+1:]...)
+		}
+
+		// If we find more query then compose query to result
+		if joint != "" {
+			r := fmt.Sprintf("%s%s", greatest, joint)
+			result = append(result, r)
+			continue
+		}
+
+		// If not. Just put it to result
+		result = append(result, greatest)
+	}
 
 	return result
 }
