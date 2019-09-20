f09e9d77727caf9c2deb7bb065f177108a8e24c6
Jian-Hua, He <game09116@gmail.com>
2019-09-20 11:02:29 +0800

    Add sorting

--- interview/minquery/minquery.go
+++ interview/minquery/minquery.go
@@ -1,5 +1,18 @@
 package minquery
 
+import (
+	"sort"
+)
+
+type ByLen []string
+
+func (s ByLen) Len() int           { return len(s) }
+func (s ByLen) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s ByLen) Less(i, j int) bool { return len(s[i]) < len(s[j]) }
+
 func generateQueries(keywords []string, maxCharLen int) []string {
-	return []string{}
+	result := []string{}
+	sort.Sort(ByLen(keywords))
+
+	return result
 }
